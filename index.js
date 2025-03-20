036fe2d2a6b5aa52f3215ba03a93b3aa15814fc5
jovangolic <118064547+jovangolic@users.noreply.github.com>
2025-03-20 19:35:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Grid from "./Grid.js";
-import Player from "./Player.js";
-import GridPosition from "./GridPosition.js";
+
 import Game from "./Game.js"; 
 
 document.getElementById("startGame").addEventListener("click", () => {
